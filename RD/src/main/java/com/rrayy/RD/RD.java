1efd4cb2865d421461feef637314f31fae84d9f5
rrayy <rrayy25809@gmail.com>
2024-05-20 22:16:33 +0900

    최대 데미지 수정 명령어 추가

--- RD/src/main/java/com/rrayy/RD/RD.java
+++ RD/src/main/java/com/rrayy/RD/RD.java
@@ -3,6 +3,8 @@ package com.rrayy.RD;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +12,7 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RD extends JavaPlugin implements Listener{
+    private int maxdamage = 15;
     public Random random = new Random();
 
     @Override
@@ -21,7 +24,7 @@ public class RD extends JavaPlugin implements Listener{
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent e) {
         if (e.getEntity() instanceof Player) {
-            int damage = random.nextInt(15);
+            int damage = random.nextInt(maxdamage);
             e.setDamage(damage);
             Player pl = (Player) e.getEntity();
             pl.sendMessage("방금 "+ChatColor.RED+damage+ChatColor.WHITE+"의 피해를 입으셨습니다");
@@ -33,15 +36,12 @@ public class RD extends JavaPlugin implements Listener{
         getLogger().info("random damage plugin이 비활성화 되었습니다");
     }
 
-    /*@Override
-    public boolean onCommand( CommandSender sender, Command command, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if ((sender instanceof Player)){
-            Player pl = (Player) sender;
-            if (!(haskit.containsKey(pl.getUniqueId().toString()))){
-                kitselector.itemSelector(pl);
-            } else sender.sendMessage("이미 아이템을 지급 받았습니다!");
-            return true;
+            maxdamage = Integer.parseInt(args[0]);
+            sender.sendMessage("이제 모든 플레이어가 최대 "+maxdamage+"만큼의 데미지를 입습니다.");
         }else sender.sendMessage("해당 명령어는 콘솔에서는 불가능 합니다");
         return false;
-    }*/
+    }
 }
